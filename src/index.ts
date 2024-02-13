9ca8e0e3b78470a231cfb6aac653dd6068fb9fb9
polcondal <polcondalgarcia@gmail.com>
2024-02-13 18:16:12 +0100

    feat: Implement EJS with layouts & public

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+import express from "express";
+import expressLayouts from 'express-ejs-layouts';
 import { config } from "dotenv";
 import audit from "express-requests-logger";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 config();
 
 // import { Client, LegacyClient, Auth } from 'osu-web.js';
 // Client for the current API (API v2)
 // const client = new Client('eUPnOYKsnu4dBD6BJzjtsrtFpf91r7LFK7MTkbAa');
 
-import express, { Request, Response } from "express";
 import { RiotPUUIDByTagName, RiotCallExample, Cs2CallExample } from "./calls.js";
 import mainRouter from "./routes/mainRouter.js";
 
@@ -15,6 +20,13 @@ const app = express();
 console.log("Initializing server...");
 
 app.use(audit());
+
+console.log(__dirname + '../public');
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
 
 // server.get('/hola', async (req: Request, res: Response): Promise<void> => {
 //     res.sendStatus(200);
@@ -41,7 +53,7 @@ app.use(audit());
 // console.log(JSON.stringify(cs2));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index', { title: "Homepage" });
 });
 
 app.use(mainRouter);
