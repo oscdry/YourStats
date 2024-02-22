64a2758f6aea669348c166f480ae35f9545778f0
JoanLinares <joan.linares@gracia.lasalle.cat>
2024-02-22 19:05:01 +0100

    feat: get Users maded

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -16,3 +16,35 @@ export async function createUser(req: Request, res: Response) {
         res.status(500).json({ error: 'There was an error creating the user', details: message });
     };
 }
+
+export async function getUser(req: Request, res: Response) {
+    const { id, email } = req.query; // Asume que los valores vienen como query params
+
+    try {
+        if (id) {
+            // Buscar usuario por ID
+            const doc = await firestore.collection('users').doc(String(id)).get();
+            if (!doc.exists) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+            return res.status(200).json(doc.data());
+        } else if (email) {
+            // Buscar usuario por email
+            const querySnapshot = await firestore.collection('users').where('email', '==', email).get();
+            if (querySnapshot.empty) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+            // Suponiendo que el email es único, devuelve el primer resultado
+            const userData = querySnapshot.docs[0].data();
+            return res.status(200).json(userData);
+        } else {
+            // Si no se proporciona ni id ni email
+            return res.status(400).json({ error: 'User ID or email must be provided' });
+        }
+    } catch (error) {
+        const message = (error as Error).message;
+        res.status(500).json({ error: 'An error occurred while fetching the user', details: message });
+    }
+};
+
+
