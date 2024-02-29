73d3a4ec7a2fda0ad2c3405939ffcbbd1c7f2fca
polcondal <polcondalgarcia@gmail.com>
2024-02-29 19:48:41 +0100

    feat: 404 page, user view

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,10 +1,21 @@
 import { Router, type Response, type Request } from "express";
+import { getFirebaseUserById } from "../api/services/FirebaseServices.js";
 
 const webRouter = Router();
 
+const NotFoundPage = (_req: Request, res: Response) => {
+    res.status(404).render('404.ejs', { title: "Page not found" });
+};
 
 webRouter.get("/", (_req: Request, res: Response) => {
     res.render('index', { title: "Homepage" });
+});
+
+webRouter.get("/user/:id", async (_req: Request, res: Response) => {
+    const user = await getFirebaseUserById(_req.params.id);
+    if (!user) return NotFoundPage(_req, res);
+
+    res.render('./user.ejs', { title: "User", userView: user });
 });
 
 webRouter.get("/admin", (_req: Request, res: Response) => {
