e8da43e547c7f56c6f19106a9e632c8a454353fb
kxnzen <polcondalgarcia@gmail.com>
2024-03-12 18:05:54 +0100

    feat: Implemented LoL items in view + css

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -8,9 +8,11 @@ import { errorHandler } from "../api/middlewares/errorHandler.js";
 import { UserNotFoundError } from "../api/errors/errors.js";
 import { verifyTokenOptional } from "../api/middlewares/verifyToken.js";
 import Pino from "../logger.js";
+import { lolTestData } from "../api/services/lolTestData.js";
 
 const webRouter = Router();
 
+// Not found page
 const NotFoundPage = (_req: Request, res: Response) => {
     res.status(404).render('404.ejs', { title: "Page not found" });
 };
@@ -42,9 +44,14 @@ webRouter.get("/lol", (_req: Request, res: Response) => {
     res.render('./lol/index.ejs', { title: "LoL" });
 });
 
+//! Testing route
+webRouter.get("/lol/stats/asd", async (_req: Request, res: Response, next: NextFunction) => {
+
+    return res.render('./lol/lol-user-stats.ejs', { title: "LoL Stats", loldata: lolTestData });
+});
+
 webRouter.get("/lol/stats/:gamename", async (_req: Request, res: Response, next: NextFunction) => {
     try {
-
         const loldata = await GetLolUserData(_req.params.gamename);
         if (!loldata) throw new UserNotFoundError();
 
@@ -53,10 +60,9 @@ webRouter.get("/lol/stats/:gamename", async (_req: Request, res: Response, next:
         return res.render('./lol/lol-user-stats.ejs', { title: "LoL Stats", loldata });
 
     } catch (error) {
-
         const message = (error as Error).message;
         const name = (error as Error).name;
-        Pino.info("Error getting stats" +  name + message);
+        Pino.info("Error getting stats" + name + message);
 
         next(error);
     }
