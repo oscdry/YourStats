8694fbca286b68aaf856b3f0ce7328c8a92c0e78
oscdry <osanz@padredamiansscc.net>
2024-04-16 17:42:42 +0200

    Fortnite Data

--- src/api/services/forniteServices.ts
+++ src/api/services/forniteServices.ts
@@ -5,7 +5,7 @@ const FORTNITE_API_ENDPOINT = 'https://fortnite-api.com/v2/stats/br/v2/';
 import { config } from 'dotenv';
 config();
 
-export const getFortniteShopData = async (language : string ) => {
+export const getFortniteShopData = async (language: string) => {
     const API_ENDPOINT = 'https://fortnite-api.com/v2/shop/br';
 
     const url = `${API_ENDPOINT}?language=${language}`;
@@ -18,28 +18,31 @@ export const getFortniteShopData = async (language : string ) => {
     }
 
     const json = await response.json();
+    
     return json;
 };
 
 export const getFortniteStatsByAccountId = async (name, accountType = 'epic', timeWindow = 'lifetime', image = 'none'): Promise<any> => {
-    
-    const apiKey = process.env.FORTNITE_API_KEY!;
 
-    const url = `${FORTNITE_API_ENDPOINT}?name=${name}&accountType=${accountType}&timeWindow=${timeWindow}&image=${image}`;
+    const apiKey = process.env.FORTNITE_API_KEY!;
     
+    const url = `${FORTNITE_API_ENDPOINT}?name=${name}&accountType=${accountType}&timeWindow=${timeWindow}&image=${image}`;
+    console.log(url);
     const response = await fetch(url, {
         headers: {
-            'Authorization': process.env.FORTNITE_API_KEY!
+            'Authorization': apiKey
         }
     });
     if (response.status !== 200) {
         console.log('Error:', response.statusText);
         return null;
     }
-    
+
     const json = await response.json();
-    return json;    
+    
+    return json;
 };
-const accountId = "Duality";
+
+const accountId = "Kosma Hris";
 const language = "en";
-console.log(await getFortniteShopData(language));
+console.log( JSON.stringify(await getFortniteStatsByAccountId(accountId)));   
